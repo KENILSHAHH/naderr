a71a9dd349e5e6e237525dfdc20f97fa63c87dd1
kenilshahh <kenilshah1505@gmail.com>
2023-02-11 22:23:33 +0530

    new app

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,27 +1,26 @@
 require("@nomiclabs/hardhat-waffle");
 const fs = require('fs');
+const privateKey=fs.readFileSync(".secret").toString();
 // const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
-
+require('dotenv').config();
 module.exports = {
-  defaultNetwork: "hardhat",
+  defaultNetwork:  "hardhat",
   networks: {
     hardhat: {
       chainId: 1337
     },
-    /*
-    mumbai: {
-      // Infura
-      // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
-      url: "https://rpc-mumbai.matic.today",
-      accounts: [process.env.privateKey]
+    
+    // mumbai: {
+    //   // Infura
+    //   // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
+    //   url: "https://rpc-mumbai.matic.today",
+    //   accounts: [process.env.privateKey]
+    // },
+    polygon_mumbai: {
+      url: "https://eth-goerli.g.alchemy.com/v2/bh4pS5ox6dbhgJdlc3bxBnBvuUAHU7sH",
+      accounts: [process.env.PRIVATE_KEY]
     },
-    matic: {
-      // Infura
-      // url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
-      url: "https://rpc-mainnet.maticvigil.com",
-      accounts: [process.env.privateKey]
-    }
-    */
+    
   },
   solidity: {
     version: "0.8.4",
